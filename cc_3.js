a3f47994e264ffe44cb6a134992897c24f8a4a86
fabianarequena <fabianarequena@usf.edu>
2025-02-01 21:42:26 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -1,7 +1,17 @@
 // Task 1: Product Price Management
 
 let prices = [100, 200, 300, 400, 500];
-prices.push(250);  // Adding a new price to the array
-prices.shift();    // Removing the first price from the array
+prices.push(250);  // Added a new price to the array
+prices.shift();    // Removed the first price from the array
 
 console.log("Updated Prices:", prices);
+
+
+// Task 2: Modifying Customer Orders
+
+let orders = [10, 20, 30, 40, 50];
+orders[2] += 5; // Increased the third order by 5, so from 30 to 35
+let totalOrders = orders.reduce((total, num) => total + num, 0); // Calculated total # of orders
+console.log("Updated Orders:", orders);
+console.log("Total Order Count:", totalOrders);
+ 
